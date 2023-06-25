bf52e0119ca8c2cbd1820ec207447640199aabeb
Stas Pochynok <staspochyn@gmail.com>
2023-06-25 15:22:39 -0400

    dashboard + demo pages

---
+++ app/(demo)/demo/page.js
@@ -0,0 +1,16 @@
+import Grid from "@/components/Grid";
+import WidgetsList from "@/components/WidgetsList";
+
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+
+function Home() {
+  return (
+    <div>
+      <WidgetsList />
+      <Grid />
+    </div>
+  );
+}
+
+export default Home;
